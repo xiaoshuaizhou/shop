66f83ce281a47ac00f8d110a580dcd7ab90525f7
zhouxiaoshuai <zhoubsshuai@163.com>
2018-04-25 19:06:34 +0800

    '集成支付'

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.16 on 2018-04-25 14:42:51.
+ * Generated for Laravel 5.6.16 on 2018-04-25 18:11:41.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- app/Models/Home/Pay.php
+++ app/Models/Home/Pay.php
@@ -3,11 +3,22 @@
 namespace App\Models\Home;
 
 use Illuminate\Database\Eloquent\Model;
+use Payment\Client\Charge;
+use Payment\Common\PayException;
 
 class Pay extends Model
 {
     public static function Alipay($orderid)
     {
+        date_default_timezone_set('Asia/Shanghai');
+
+        try {
+            $str = Charge::run('ali_web');
+        } catch (PayException $e) {
+            echo $e->errorMessage();
+            exit;
+        }
+
 
     }
 }
