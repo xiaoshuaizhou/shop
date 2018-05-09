8b74fc20380cbede9f4fe82b38d440c8f25e9bf3
zhouxiaoshuai <zhouxiaoshuai3@gmail.com>
2018-05-09 23:09:17 +0800

    添加alipayController

---
+++ app/Http/Controllers/Home/AliPayController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AliPayController extends Controller
+{
+    public function index()
+    {
+        dd('this is alipay page!!');
+    }
+}
